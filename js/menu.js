356dfeb2e8d5c86a42fee9474859c83c354f8e61
DonReed <dreedsamoa1996@gmail.com>
2023-09-11 20:58:42 +1300

    fixed temp switch between Celcius and Fahr"

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+//import { add } from "./calculate.js"
+
 document.querySelector('#open-nav-menu').addEventListener('click', function () {
     document.querySelector('header nav .wrapper').classList.add('nav-open');
 });
@@ -8,6 +10,41 @@ document.querySelector('#close-nav-menu').addEventListener('click', function () 
 
 
 //Greeting section
-const greet = 'Good Morning';
+const greet = 'Good Afternoon';
 const condition = 'Sunny';
-const location = 'Samoa';
+const userLocation = 'New Zw';
+let temperature = 98.80000996;
+
+document.querySelector('.weather-group').addEventListener('click', function(e) {
+    let temp = e.target.id;
+    let weatherText = '';
+    if (temp == 'fahr') {
+        weatherText = `The weather is ${condition} in ${userLocation} and it's ${celToFar(temperature).toFixed(1)}°F outside`;
+    } else {
+        weatherText = `The weather is ${condition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside`;
+    }
+    updateElement(`#weather`, weatherText);
+});
+
+
+
+function updateElement(id, text) {
+    document.querySelector(id).innerHTML = text
+}
+
+function celToFar (cel) {
+    return ((cel * 9/5) + 32); 
+}
+
+updateElement('#greeting', greet)
+
+
+// function maxInArr (arr) {
+//     return Math.max(...arr)
+// }
+// array = [1,9,10,50,2,0]
+// console.log(maxInArr(array))
+// function addUp (a, b) {
+//     return a+b;
+// }
+// console.log(addUp(100, 9))
